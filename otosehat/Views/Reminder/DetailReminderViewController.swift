dafca5214b28770b26274af6ef6f2236c779803a
hansrichard2000 <56462883+hansrichard2000@users.noreply.github.com>
2022-04-12 17:38:07 +0700

    Input foundation for edit

--- otosehat/Views/Reminder/DetailReminderViewController.swift
+++ otosehat/Views/Reminder/DetailReminderViewController.swift
@@ -22,10 +22,50 @@ class DetailReminderViewController: UIViewController {
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblDescription: UILabel!
     
+    private var models = [MyReminder]()
+    
     @IBAction func didEditBtnTapped(_ sender: Any) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "edit") as? EditReminderViewController else{
             return
         }
+//        vc.title = "Edit Reminder"
+//        vc.navigationItem.largeTitleDisplayMode = .never
+//        vc.completion = {
+//            title, body, date in
+//            DispatchQueue.main.async{
+////                self.navigationController?.popToRootViewController(animated: true)
+//                vc.dismiss(animated: true) //buat nutup modal after save data
+//
+//                let new = MyReminder(title: title, body: body, date: date, identifier: "id_\(title)")
+////                self.models.(new)
+//
+////                self.createItem(title: title, body: body, date: date)
+//
+//                let content = UNMutableNotificationContent()
+//                content.title = title
+//                content.sound = .default
+//                content.body = body
+//
+//                let targetDate = date
+//                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+//
+//                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+//                UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+//                    if error != nil{
+//                        print("something went wrong...")
+//                    }
+//                })
+//
+//                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {success, error in
+//                    if success{
+//                        //schedule test
+////                        self.scheduleTest()
+//                    }else if error != nil{
+//                        print("error occurred")
+//                    }
+//                })
+//            }
+        
     }
     
     var img = UIImage()
--- otosehat/Views/Reminder/EditReminderViewController.swift
+++ otosehat/Views/Reminder/EditReminderViewController.swift
@@ -7,23 +7,57 @@
 
 import UIKit
 
-class EditReminderViewController: UIViewController {
+class EditReminderViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBOutlet weak var descField: UITextField!
+    
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    public var completion: ((String, String, Date) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleField.delegate = self
+        descField.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didCancelTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func didSaveButtonTapped(_ sender: Any) {
+        if let titleText = titleField.text, !titleText.isEmpty,
+           let bodyText = descField.text, !bodyText.isEmpty{
+            
+            let targetDate = datePicker.date
+            completion?(titleText, bodyText, targetDate)
+            
+        }
+    }
+    
+    @objc func didTapSaveButton(){
+        if let titleText = titleField.text, !titleText.isEmpty,
+           let bodyText = descField.text, !bodyText.isEmpty{
+            
+            let targetDate = datePicker.date
+            
+            completion?(titleText, bodyText, targetDate)
+            
+        }
+    }
+    
+    internal func textFieldShouldReturn(  _ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
